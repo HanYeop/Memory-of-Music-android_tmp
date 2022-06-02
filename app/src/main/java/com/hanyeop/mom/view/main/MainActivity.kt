4161386c2b63610a0137c15ea07ae863c7b93053
super7615 <super7615@naver.com>
2022-06-02 22:40:04 +0900

    [ADD] : music, album, main, setting 프래그먼트 추가

---
+++ app/src/main/java/com/hanyeop/mom/view/main/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hanyeop.mom.view.main
+
+import com.hanyeop.mom.R
+import com.hanyeop.mom.base.BaseActivity
+import com.hanyeop.mom.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun init() {
+
+    }
+}
---
+++ app/src/main/java/com/hanyeop/mom/view/main/setting/SettingFragment.kt
@@ -0,0 +1,12 @@
+package com.hanyeop.mom.view.main.setting
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hanyeop.mom.R
+
+class SettingFragment : Fragment() {
+
+}
